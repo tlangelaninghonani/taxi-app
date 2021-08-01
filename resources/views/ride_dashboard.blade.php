4d503f5f09d54b94582f20c1fdafba931246d231
Tlangelani Nghonani <nghonanitlangelani772@gmail.com>
2021-08-01 18:57:08 +0200

    changes

--- resources/views/ride_dashboard.blade.php
+++ resources/views/ride_dashboard.blade.php
@@ -234,7 +234,8 @@
                 const map = new google.maps.Map(document.getElementById("map"), {
                 center: { lat: -33.8688, lng: 151.2195 },
                 zoom: 13,
-                mapId: "4cce301a9d6797df"
+                mapId: "4cce301a9d6797df",
+                disableDefaultUI: true,
                 });
 
                 let directionsService = new google.maps.DirectionsService();
